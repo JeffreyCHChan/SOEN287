781940c0ec5fec5db873d6ff90cef81a3712115f
Quikol <chan.jch19@gmail.com>
2020-07-26 09:27:20 -0400

    hidden more description
    added the ability to change the visibility

---
+++ product-pages.js
@@ -0,0 +1,8 @@
+function hiddenSwitch(){
+    var x = document.getElementById("moreDescription");
+    if (x.style.display === "none") {
+     x.style.display = "block";
+    } else {
+    x.style.display = "none";
+    }
+}
