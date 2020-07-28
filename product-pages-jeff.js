8c904e42dd5a46516b4834259bf5464d5b337e0c
Quikol <chan.jch19@gmail.com>
2020-07-28 10:00:15 -0400

    quantity change and sub-total

---
+++ product-pages-jeff.js
@@ -0,0 +1,73 @@
+function hiddenSwitch(){
+    var x = document.getElementById("moreDescription"); //creating a instance variable looking for the id moreDescription
+    if (x.style.display === "none") {
+     x.style.display = "block";//if the variable carries the attribute of display is "none" then we set it to block which reveals the code
+    } else {
+    x.style.display = "none"; //else then set the attribute to none which hides it
+    }
+}
+function add(){
+    var quantity = document.getElementById("quantity").value; //takes the value from the input
+    //var cap = document.
+    //if(){} //maybe check if it is less than the current inventory
+    quantity++;
+    document.getElementById("quantity").value = quantity;
+}
+
+function subtract(){
+    var quantity = document.getElementById("quantity").value; //takes the value from the input
+    if(quantity>1){
+    quantity--;
+    document.getElementById("quantity").value = quantity;
+    }
+    else{
+        document.getElementById("quantity").value = quantity;
+    }
+}
+function popUp(){
+    alert("Added to cart");
+}
+function addToCart(){ 
+    var node = document.getElementById(""); //change myList to the id of the shopping cart
+    var textnode = document.createElement("li");         // Create a node
+    var quantity = document.getElementById("quantity").value; //takes the value from the input
+    var productName = document.getElementById("productName").innerHTML //takes the text value
+    var productNumber = document.getElementById("productNumber").innerHTML
+    var price = document.getElementById("price").innerHTML
+    textnode.innerHTML = "Item: "+ productName+  " &nbsp;Price: "+price+"$,   &nbsp; Id:"+productNumber+",  &nbsp; Quantity: " + quantity //parse double to get the price maybe
+    node.appendChild(textnode);                              // Append the text to <li>
+    document.getElementById("myList").appendChild(node); //change myList to the id of the shopping cart
+}
+
+function cost(){
+    var quantity = document.getElementById("quantity").value;
+    var price = document.getElementById("price").innerHTML;
+    var subTotal = quantity * (parseFloat(price)); 
+    document.getElementById("sub-total").innerHTML = ("<span class=Quantity><b>Sub-Total: </b></span> $"+subTotal.toFixed(2));//toFixed limits the digits after the decimals
+}
+
+
+function editProduct(){
+
+    var productName;
+    var quantity;
+    var averageCost;
+    var units;
+    var price;
+    var productDescription;
+    var productBrand;
+    var countryOfOrigin;
+    var productNumber;
+}
+
+function addProduct(){
+    var productName;
+    var quantity;
+    var averageCost;
+    var units;
+    var price;
+    var productDescription;
+    var productBrand;
+    var countryOfOrigin;
+    var productNumber;
+}
--- product-pages.js
+++
@@ -1,8 +0,0 @@
-function hiddenSwitch(){
-    var x = document.getElementById("moreDescription");
-    if (x.style.display === "none") {
-     x.style.display = "block";
-    } else {
-    x.style.display = "none";
-    }
-}
