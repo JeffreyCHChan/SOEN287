7483bb9627aee12fba07665aa365dbe4eee4865e
naregschoolwork <naregschoolwork@gmail.com>
2020-08-02 11:35:33 -0400

    Done Front End

--- NaregJS.js
+++ NaregJS.js
@@ -20,9 +20,27 @@ var descInfo = document.getElementsByClassName("more");
 }
 
 //Add to Cart button implementation to show us an option to increase or decrease the amount of the item to be purchased.
-let itemList = [];
+let itemList = (JSON.parse(localStorage.getItem('itemList')) || []);
+
 const itemCartDOM = document.querySelector('.itemCartInfo');
 const aTCButtonDOM = document.querySelectorAll('[data-action="AddToCart"]');
+
+if(itemList.length > 0){
+    itemList.forEach(itemInfo => {
+        const item = itemInfo;
+
+        insertItem(item);
+
+                //For increasing the amount of the item to be purchased.
+                const bakeryItem = itemCartDOM.querySelectorAll('.item_');
+                bakeryItem.forEach(individualInfo => {
+                    if(individualInfo.querySelector('.item_name').innerText === item.name) {
+                        buttonHandler(item);
+                    }
+                });
+    });
+}
+
 aTCButtonDOM.forEach(itemDescription => {
     itemDescription.addEventListener('click' , () => {
         const itemDOM = itemDescription.parentNode;
@@ -35,59 +53,74 @@ aTCButtonDOM.forEach(itemDescription => {
         //Making sure that the name and the price will only appear once when clicking add to cart.
         const inCheck = (itemList.filter(itemInfo => (itemInfo.name === item.name)).length > 0);
         if (inCheck === false){
-                itemCartDOM.insertAdjacentHTML('beforeend' , `
-        <div class="item_">
+            insertItem(item);
+
+        itemList.push(item);
+        //To allow refreshing the page and keeping the content.
+        localStorage.setItem('itemList' , JSON.stringify(itemList) );
+        buttonHandler(item);
+
+        
+        }
+    }); 
+
+});
+//Creating functions that will be used more than once.
+function insertItem(item) {
+    itemCartDOM.insertAdjacentHTML('beforeend' , `
+    <div class="item_">
         <h3 class="item_name">${item.name}</h3>
         <h3 class="item_price">${item.price}</h3>
         <button class="cart-btn" data-action="IncreaseItem" style="width: 5%; height: 3%;">&plus;</button>
         <h3 class="item_quantity">${item.quantity}</h3>
         <button class="cart-btn" data-action="DecreaseItem" style="width: 5%; height: 3%;">&minus;</button>
         <button class="cart-btn" data-action="RemoveItem" style="width: 5%; height: 3%;background-color:red">&times;</button>
-        </div>
-        `); 
-        itemList.push(item);    
+    </div>
+    `); 
+}
 
-        //For increasing the amount of the item to be purchased.
-        const bakeryItem = itemCartDOM.querySelectorAll('.item_');
-        bakeryItem.forEach(individualInfo => {
-            if(individualInfo.querySelector('.item_name').innerText === item.name) {
-            individualInfo.querySelector('[data-action="IncreaseItem"]').addEventListener('click', () => {
-                itemList.forEach(itemInfo => {
-                    if(itemInfo.name === item.name) {
-                        individualInfo.querySelector('.item_quantity').innerText = ++itemInfo.quantity;
-                    }
-                });
+function buttonHandler(item) {
+    //For increasing the amount of the item to be purchased.
+    const bakeryItem = itemCartDOM.querySelectorAll('.item_');
+    bakeryItem.forEach(individualInfo => {
+        if(individualInfo.querySelector('.item_name').innerText === item.name) {
+        individualInfo.querySelector('[data-action="IncreaseItem"]').addEventListener('click', () => {
+            itemList.forEach(itemInfo => {
+                if(itemInfo.name === item.name) {
+                    individualInfo.querySelector('.item_quantity').innerText = ++itemInfo.quantity;
+                    localStorage.setItem('itemList' , JSON.stringify(itemList) );
+                }
             });
-            //For decreasing the amount of the item to be purchased.
-            individualInfo.querySelector('[data-action="DecreaseItem"]').addEventListener('click', () => {
-                itemList.forEach(itemInfo => {
-                    if(itemInfo.name === item.name) {
-                        if(itemInfo.quantity > 1){ 
-                        individualInfo.querySelector('.item_quantity').innerText = --itemInfo.quantity;
-                    } else {
-                        individualInfo.remove();
-                        itemList = itemList.filter(itemInfo => itemInfo.name !== item.name);
-                        }
-                    }
-                });
-            });
-            //For the removing button
-            individualInfo.querySelector('[data-action="RemoveItem"]').addEventListener('click', () => {
-                itemList.forEach(itemInfo => {
-                    if(itemInfo.name === item.name) {
-                        individualInfo.remove();
-                        itemList = itemList.filter(itemInfo => itemInfo.name !== item.name);
-                    }
-                });
-            });
-
-
-        }
         });
-        }
-    }); 
+        //For decreasing the amount of the item to be purchased.
+        individualInfo.querySelector('[data-action="DecreaseItem"]').addEventListener('click', () => {
+            itemList.forEach(itemInfo => {
+                if(itemInfo.name === item.name) {
+                    if(itemInfo.quantity > 1){ 
+                    individualInfo.querySelector('.item_quantity').innerText = --itemInfo.quantity;
+                    localStorage.setItem('itemList' , JSON.stringify(itemList) );
+                } else {
+                    individualInfo.remove();
+                    itemList = itemList.filter(itemInfo => itemInfo.name !== item.name);
+                    localStorage.setItem('itemList' , JSON.stringify(itemList) );
+                    }
+                }
+            });
+        });
+        //For the removing button
+        individualInfo.querySelector('[data-action="RemoveItem"]').addEventListener('click', () => {
+            itemList.forEach(itemInfo => {
+                if(itemInfo.name === item.name) {
+                    individualInfo.remove();
+                    itemList = itemList.filter(itemInfo => itemInfo.name !== item.name);
+                    localStorage.setItem('itemList' , JSON.stringify(itemList) );
+                }
+            });
+        });
 
-});
 
-
+    }
+    });
+}
+ 
 
