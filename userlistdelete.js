803fcf1d34e5bdabe42fe989ac1ec4fc7888590a
giroux2000 <67913484+giroux2000@users.noreply.github.com>
2020-08-02 20:09:18 -0400

    Add files via upload

--- userlistdelete.js
+++ userlistdelete.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-=======
-userdelete
-
->>>>>>> master
 //Maxime Giroux 40157483
 
 function deleteRow(r) {
     var i = r.parentNode.parentNode.rowIndex;
     document.getElementById("userlist").deleteRow(i);
-<<<<<<< HEAD
   }
 
-=======
-  }
->>>>>>> master
